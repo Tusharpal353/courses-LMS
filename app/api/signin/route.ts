872fcb43e43d222a4a32366d5c718be866451f80
Tushar Pal <Tusharpal353@gmail.com>
2024-12-13 13:12:36 +0530

    hashed password and created jwt

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -1,7 +1,10 @@
 import User from "@/models/users/user"
 import connectToDb from "@/lib/db"
 import { NextResponse, NextRequest } from 'next/server';
-
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import Signin from '../../signin/page';
+const SECRET_KEY = process.env.JWT_SECRET
 
 export async function POST(req:NextRequest) {
     try {
@@ -14,11 +17,29 @@ export async function POST(req:NextRequest) {
             )
         }
 
-        if(userExists.password !== password){
+        const isPasswordValid = await bcrypt.compare(password,userExists.password)
+
+
+        if(!isPasswordValid){
                 return NextResponse.json({
                     error:"password is not correct"
                 })
         }
+        const token = jwt.sign(
+            {
+                id:userExists.id,
+                email:userExists.email,
+                role:userExists.role
+            },
+            SECRET_KEY,
+            {
+                expiresIn:"1h"
+            }
+        )
+
+
+
+
         return NextResponse.json(
             { message: "Login successful" },
             { status: 200 }
--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,33 +1,34 @@
-import User from "@/models/users/user"
-import connectToDb from "@/lib/db"
-import { NextResponse, NextRequest } from 'next/server';
-
-
-export async function POST(req:NextRequest) {
-    try {
-        connectToDb()
-        const {username,email,password}= await req.json()
-        const userExists = await User.findOne({email})
-        if(userExists){
-            return NextResponse.json(
-              {  error:"user already exists in db try new mail"}
-            )
-        }
-        const newUser = new User({
-                username,email,password
-        })
-        await newUser.save( )
-return NextResponse.json({
-    message:"user created",
-    status:201  
-})
-        
-    } catch (err:any) {
-        return NextResponse.json({
-            error:err.message,
-            status:500
-        })
-        
+import User from "@/models/users/user";
+import connectToDb from "@/lib/db";
+import { NextResponse, NextRequest } from "next/server";
+import bcrypt from "bcryptjs";
+export async function POST(req: NextRequest) {
+  try {
+    connectToDb();
+    const { username, email, password } = await req.json();
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return NextResponse.json({
+        error: "user already exists in db try new mail",
+      });
     }
-    
+
+
+    const hasedPassword = await bcrypt.hash(password,10)
+    const newUser = new User({
+      username,
+      email,
+      password:hasedPassword,
+    });
+    await newUser.save();
+    return NextResponse.json({
+      message: "user created",
+      status: 201,
+    });
+  } catch (err: any) {
+    return NextResponse.json({
+      error: err.message,
+      status: 500,
+    });
+  }
 }
